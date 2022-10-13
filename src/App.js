0523a4fd3d2ba9b9c9354b5f47421530a8eee444
Nafis-Fahim <nafisf96@gmail.com>
2022-10-14 01:36:43 +0600

    get form value

--- src/App.js
+++ src/App.js
@@ -4,12 +4,29 @@ import app from "./Firebase/firebase.init";
 
 const auth = getAuth(app);
 function App() {
+
+  const handleToGetValue = (event) =>{
+    event.preventDefault();
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+    console.log(email, password);
+  }
+  const handleOnChange = (e) =>{
+    console.log(e.target.value);
+  }
+
+  const handlePassChange = e =>{
+    console.log(e.target.value);
+  }
   return (
-    <div className="App">
-      <form>
-        <input type="email" name="" id="" placeholder="Email" />
+    <div className="container mx-auto mt-10 bg-slate-200 rounded-lg pt-5 shadow-lg shadow-slate-700">
+      <h1 className="text-3xl mx-5 text-blue-600 font-bold">Email & Password Authentication</h1>
+      <form className="mt-5" onSubmit={handleToGetValue}>
+        <input onChange={handleOnChange} className="w-auto mx-5 py-1 px-3" type="email" name="email" id="" placeholder="Email" />
         <br />
-        <input type="password" name="" id="" placeholder="Password" />
+        <input onChange={handlePassChange} className="mt-5 mx-5 py-1 px-3" type="password" name="password" id="" placeholder="Password" />
+        <br />
+        <button className="mt-3 mx-5 p-2 mb-5 bg-blue-500 rounded-md text-white" type="submit">Register</button>
       </form>
     </div>
   );
